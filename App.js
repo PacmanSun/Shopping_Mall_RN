f7b423e5f204323aace7156ce31e5cb8c0b4419e
孙岦 <sunli@promote.cache-dns.local>
2018-05-03 20:50:30 +0800

    V0.2优化searchBar，scrollView，listView。

--- App.js
+++ App.js
@@ -17,8 +17,10 @@ import {
   ListView,
   Alert,
   TouchableHighlight,
-  StatusBar
+  StatusBar,
+  RefreshControl
 } from 'react-native';
+import { Image } from 'react-native';
 
 
 // const instructions = Platform.select({
@@ -27,11 +29,14 @@ import {
 //   android: 'Double tap R on your keyboard to reload,\n' +
 //     'Shake or press menu button for dev menu',
 // });
+const circleSize = 8;
+const circleMargin = 5;
 
 type Props = {};
 export default class App extends Component<Props> {
   constructor(Props){
     super(Props);
+
     const ds = new ListView.DataSource({
       rowHasChanged:(r1,r2)=> r1 !== r2
     });
@@ -39,17 +44,95 @@ export default class App extends Component<Props> {
     this.state={
       currentPage: 0,
       dataSource:ds.cloneWithRows([
-        '商品1',
-        '商品2',
-        '商品3',
-        '商品4',
-        '商品5',
-        '商品6',
-        '商品7',
-        '商品8',
-        '商品9',
-        '商品10',
-      ])
+        // '商品1',
+        // '商品2',
+        // '商品3',
+        // '商品4',
+        // '商品5',
+        // '商品6',
+        // '商品7',
+        // '商品8',
+        // '商品9',
+        // '商品10',
+        {
+          image:require('./images/product-image-01.jpg'),
+          title:'商品1',
+          subTitle:'描述1',
+        },
+        {
+          image:require('./images/product-image-01.jpg'),
+          title:'商品2',
+          subTitle:'描述2',
+        },
+        {
+          image:require('./images/product-image-01.jpg'),
+          title:'商品3',
+          subTitle:'描述3',
+        },
+        {
+          image:require('./images/product-image-01.jpg'),
+          title:'商品4',
+          subTitle:'描述4',
+        },
+        {
+          image:require('./images/product-image-01.jpg'),
+          title:'商品5',
+          subTitle:'描述5',
+        },
+        {
+          image:require('./images/product-image-01.jpg'),
+          title:'商品6',
+          subTitle:'描述6',
+        },
+        {
+          image:require('./images/product-image-01.jpg'),
+          title:'商品7',
+          subTitle:'描述7',
+        },
+        {
+          image:require('./images/product-image-01.jpg'),
+          title:'商品8',
+          subTitle:'描述8',
+        },
+        {
+          image:require('./images/product-image-01.jpg'),
+          title:'商品9',
+          subTitle:'描述9',
+        },
+        {
+          image:require('./images/product-image-01.jpg'),
+          title:'商品10',
+          subTitle:'描述10',
+        },
+      ]),
+      advertisements:[
+        {
+          // title:'广告1',
+          // backgroundColor:'gray',
+          // url:'https://manhua.qpic.cn/vertical/0/17_16_53_938f9aa69728fd462a62074b81f0f3fb.jpg/420',
+          image:require('./images/advertisement-image-01.jpg'),
+          title:'商品1',
+          subTitle:'描述1',
+        },
+        {
+          // title:'广告2',
+          // backgroundColor:'orange',
+          // url:'https://manhua.qpic.cn/vertical/0/21_15_30_e74fc730f5b060f35c6eb0eeb082478f_1498030250417.jpg/420',
+          image:require('./images/advertisement-image-02.jpg'),
+          title:'商品2',
+          subTitle:'描述2',
+        },
+        {
+          // title:'广告3',
+          // backgroundColor:'yellow',
+          // url:'https://manhua.qpic.cn/vertical/0/18_11_45_d223b0e7004fcfac7eb5531ba2490348_1503027925211.jpg/420',
+          image:require('./images/advertisement-image-03.jpg'),
+          title:'商品3',
+          subTitle:'描述3',
+        },
+      ],
+      searchText:'',
+      refreshing:false,
     };
   }
 
@@ -76,13 +159,65 @@ export default class App extends Component<Props> {
 
   _renderRow(rowData){
     return (
-      <View style={styles.rows}>
-        <Text>{rowData}</Text>
-      </View>
+      <TouchableHighlight onPress={()=>Alert.alert('你单击了商品',null,null)}>
+        <View style={styles.rows}>
+          <Image source={rowData.image}
+            style={styles.productImage}
+          >
+          </Image>
+          <View style={styles.productText}>
+            <Text style={styles.productTitle}>
+              {rowData.title}
+            </Text>
+            <Text style={styles.productSubTitle}>
+              {rowData.subTitle}
+            </Text>
+          </View>
+        </View>
+      </TouchableHighlight>
     );
   }
 
+  _renderSeperator(sectionID, rowID){
+    // console.log(`${sectionID}-${rowID}`)
+    return(
+      <View key={`${sectionID}-${rowID}`}
+        style={styles.divider}
+      >
+      </View>
+    )
+  }
+
+  _renderRefreshControl(){
+    return(
+      <RefreshControl refreshing={this.state.refreshing}
+        onRefresh={this._onRefresh.bind(this)}
+        tintColor={'#FF0000'}
+        title={'正在刷新数据，请稍后...'}
+        titleColor={'#0000FF'}
+      >  
+      </RefreshControl>
+
+    )
+  }
+
+  _onRefresh() {
+    this.setState({refreshing: true});
+    setTimeout(() => {
+      const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});      
+      const products = Array.from(new Array(10)).map((value,index)=>({
+        image:require('./images/product-image-01.jpg'),
+        title:'新商品'+index,
+        subTitle:'新商品描述'+index,
+      }));
+      this.setState({refreshing: false,dataSource:ds.cloneWithRows(products)});
+    },2000);
+  }
+
   render() {
+    const advertisementCount=this.state.advertisements.length//指示器圆点数
+    const indicatorWidth = circleSize*advertisementCount + circleMargin*advertisementCount*2
+    const left=(Dimensions.get('window').width-indicatorWidth)/2
     return (
       <View  style={styles.container}>
         <StatusBar backgroundColor={'blue'}// 设置背景色为蓝色
@@ -95,12 +230,13 @@ export default class App extends Component<Props> {
 
           <TextInput style={styles.input} 
             placeholder="搜索商品"
+            onChangeText={(text)=>{this.setState({searchText:text})}}
           >
           </TextInput>
 
           <Button style={styles.button}
             title=" 搜 索 "
-            onPress={()=>Alert.alert('你单击了搜索按钮',null,null)}
+            onPress={()=>Alert.alert('搜索内容'+this.state.searchText,null,null)}
           >
           </Button>
 
@@ -113,42 +249,45 @@ export default class App extends Component<Props> {
             showsHorizontalScrollIndicator={false}//不显示横向滚动条
             pagingEnabled={true}//分页
           >
-            <TouchableHighlight onPress={()=>Alert.alert('你单击了轮播广告',null,null)}>
-              <Text style={{
-                width:Dimensions.get('window').width,
-                height:180,
-                backgroundColor:'gray',
-                }}
-              >广告1
-              </Text>
-            </TouchableHighlight>
-
-            <TouchableHighlight onPress={()=>Alert.alert('你单击了轮播广告',null,null)}>
-            <Text style={{
-              width:Dimensions.get('window').width,
-              height:180,
-              backgroundColor:'orange',
-              }}
-            >广告2
-            </Text>
-            </TouchableHighlight>
-
-            <TouchableHighlight onPress={()=>Alert.alert('你单击了轮播广告',null,null)}>
-            <Text style={{
-              width:Dimensions.get('window').width,
-              height:180,
-              backgroundColor:'yellow',
-              }}
-            >广告3
-            </Text>
-            </TouchableHighlight>
-
+            {
+              this.state.advertisements.map(
+                (advertisement,index)=>{
+                  return (
+                    <TouchableHighlight key={index}
+                      onPress={()=>Alert.alert('你单击了轮播广告',null,null)}
+                    >
+                      <Image style={styles.advertismentContent}
+                        // source={{uri:advertisement.url}}
+                        source={advertisement.image}
+                      >
+                      </Image>
+                    </TouchableHighlight>
+                  )
+                }
+              )
+            }
           </ScrollView>
+          <View style={[styles.indicator,{left:left}]}>
+            {this.state.advertisements.map(
+              (advertisement,index)=>{
+                return (
+                  <View key={index}
+                    style={(index===this.state.currentPage)?styles.circleSelected:styles.circle}
+                  >
+                  </View>
+                )
+              }
+            )
+          }
+          </View>
         </View>
 
         <View style={styles.products}>
           <ListView dataSource={this.state.dataSource}
             renderRow={this._renderRow}
+            style={{backgroundColor:'white',width:Dimensions.get('window').width}}
+            renderSeparator={this._renderSeperator}
+            refreshControl={this._renderRefreshControl()}
           >
           </ListView>
         </View>
@@ -180,6 +319,7 @@ const styles = StyleSheet.create({
     flex: 1,
     borderColor: 'gray',
     borderWidth: 2,
+    borderRadius: 10,
   },
   button:{
     flex: 1,
@@ -199,8 +339,61 @@ const styles = StyleSheet.create({
   },
   rows: {
     height: 60,
-    justifyContent: 'center',
-    alignItems: 'center',
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    flexDirection: 'row',
+  },
+  productImage:{
+    marginLeft: 10,
+    marginRight: 10,
+    width:40,
+    height:40,
+    alignSelf: 'center',
+  },
+  productText:{
+    flexDirection: 'column',
+    marginBottom: 10,
+    marginTop:10,
+    flex: 1,
+  },
+  productTitle:{
+    fontSize: 16,
+    color:'black',
+    flex: 3,
+  },
+  productSubTitle:{
+    fontSize:14,
+    color:'gray',
+    flex: 2,
+  },
+  divider:{
+    height:1,
+    width:Dimensions.get('window').width-10,
+    marginLeft:5,
+    backgroundColor:'lightgray',
+  },
+  advertismentContent: {
+    width:Dimensions.get('window').width,
+    height:180,
+  },
+  indicator: {
+    position: 'absolute',
+    top: 160,
+    flexDirection: 'row',
+  },
+  circle: {
+    width:circleSize,
+    height:circleSize,
+    borderRadius:circleSize/2,
+    backgroundColor:'gray',
+    marginHorizontal:circleMargin,
+  },
+  circleSelected: {
+    width:circleSize,
+    height:circleSize,
+    borderRadius:circleSize/2,
+    backgroundColor:'white',
+    marginHorizontal:circleMargin,
   },
 });
 
